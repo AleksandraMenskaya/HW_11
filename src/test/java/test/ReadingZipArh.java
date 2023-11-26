5f4f7b3d2e1c3f24301690b1b343a70f7d3461b7
Юлия Вилкова <al.menskaya@gmail.com>
2023-11-26 17:55:30 +0100

    fix2_HW_11

--- src/test/java/test/ReadingZipArh.java
+++ src/test/java/test/ReadingZipArh.java
@@ -1,31 +1,67 @@
 package test;
 
-import static org.assertj.core.api.Assertions.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStreamReader;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
 public class ReadingZipArh {
     private ClassLoader classLoader = ReadingZipArh.class.getClassLoader();
-    @ParameterizedTest(name = "Для файла {0} контрольная сумма должна быть {1}")
-    @CsvSource(value = {
-            "Java_Head_Firts_Java.pdf , 577943615",
-            "CheckList XLSX.xlsx , 3269606048",
-            "CheckList CSV.csv , 3366467583"
-    })
-    void zipFileParsingTest(String fileName, Long crc) throws Exception {
+
+    @Test
+    void zipFileParsingPDFTest() throws Exception {
         try (ZipInputStream zis = new ZipInputStream(
            classLoader.getResourceAsStream("testArh.zip")
         )) {
             ZipEntry entry;
             while ((entry = zis.getNextEntry()) != null) {
-                if (entry.getName().equals(fileName)) {
-                    assertThat(entry.getCrc()).isEqualTo(crc);
+                if (entry.getName().equals("English Book.pdf")) {
+                    PDF pdf = new PDF(zis);
+                    assertThat(pdf.text).contains("A crash is when your competitor’s program dies");
                     return;
                 }
             }
-            fail("Отсутствует " + fileName);
+            fail("Отсутствует " + "English Book.pdf");
+        }
+    }
+    @Test
+    void zipFileParsingXLSXTest() throws Exception {
+        try (ZipInputStream zis = new ZipInputStream(
+                classLoader.getResourceAsStream("testArh.zip")
+        )) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().equals("CheckList XLSX.xlsx")) {
+                    XLS xls = new XLS(zis);
+                    assertThat(xls.excel.getSheetAt(0).getRow(0).getCell(0).getStringCellValue())
+                    .isEqualTo("Сайт www.salonsecret.ru");
+                    return;
+                }
+            }
+            fail("Отсутствует " + "CheckList XLSX.xlsx");
+        }
+    }
+    @Test
+    void zipFileParsingCSVTest() throws Exception {
+        try (ZipInputStream zis = new ZipInputStream(
+                classLoader.getResourceAsStream("testArh.zip")
+        )) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().equals("CheckList CSV.csv")) {
+                    CSVReader csv = new CSVReader(new InputStreamReader(zis));
+                    assertThat(csv.readNext()[0]).contains("shop.salonsecret.ru");
+                    return;
+                }
+            }
+            fail("Отсутствует " + "CheckList CSV.csv");
         }
     }
 }
---
+++ src/test/java/test/ReadingZipArhParam.java
@@ -0,0 +1,32 @@
+package test;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class ReadingZipArhParam {
+    private ClassLoader classLoader = ReadingZipArhParam.class.getClassLoader();
+    @ParameterizedTest(name = "Для файла {0} контрольная сумма должна быть {1}")
+    @CsvSource(value = {
+            "English Book.pdf , 3506028550",
+            "CheckList XLSX.xlsx , 3269606048",
+            "CheckList CSV.csv , 3366467583"
+    })
+    void zipFileParsingTest(String fileName, Long crc) throws Exception {
+        try (ZipInputStream zis = new ZipInputStream(
+           classLoader.getResourceAsStream("testArh.zip")
+        )) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().equals(fileName)) {
+                    assertThat(entry.getCrc()).isEqualTo(crc);
+                    return;
+                }
+            }
+            fail("Отсутствует " + fileName);
+        }
+    }
+}
+
